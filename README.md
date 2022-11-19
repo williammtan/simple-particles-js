8856003425c4a9d0951bb5d3e8bf38dd4f249380
WilliamTan2704 <william.h.tan27@gmail.com>
2022-11-19 14:55:28 +0700

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unit 4 FF PT
+A simple website using [particles.js](https://github.com/VincentGarreau/particles.js/)
---
+++ index.js
@@ -0,0 +1,5 @@
+// use the particleJS library
+// load the settings defined in the particles.json file
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
