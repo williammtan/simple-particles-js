f085755e7601de9bcef22c7466b805a0b227fe22
WilliamTan2704 <william.h.tan27@gmail.com>
2022-11-19 14:58:19 +0700

    add comment to explain the particlesJS.load() function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // use the particleJS library
 // load the settings defined in the particles.json file
+// the function particlesJS.load() will do everything for us, given the settings we applied to it
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
