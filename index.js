100abc684b7c5d8eb11c104f8155fa5c6340c0e6
WilliamTan2704 <william.h.tan27@gmail.com>
2022-11-21 22:40:03 +0700

    add comment to explain the new Howl() function

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 function play() {
     // use the howler.js library to play the mp3 file song.mp3
-    var song = new Howl({
-        src: ['song.mp3'],
+    var song = new Howl({ // define Howl class
+        src: ['song.mp3'], // passing in the file path to song.mp3
         volume: 0.25
     });
       
-    song.play();
+    song.play(); // play the song
 }
